dff1c8381ead4b3361a03e7490e4e4f47bc9f92b
Heikki <heikki.patsi@outlook.com>
2022-11-03 22:17:54 +0200

    Ei toimi, useState is not defined

--- src/App.js
+++ src/App.js
@@ -2,28 +2,41 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Weather from './components/Weather';
 
-const API_URL = 'https://api.openweathermap.org/data/2.5/weather?';
-const ICON_URL = 'http://openweathermap.org/img/wn/';
-const API_KEY = 'API_KEY_HERE';
-
 
 function App() {
-  if (isLoading) {
-      return <p>Loading...</p>;
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+      }, (error) => {
+        alert(error);
+      })
     }
     else {
-      return (
-        <div className="content">
-          <h3>Your position</h3>
-          <p>
-            Position:&nbsp;
-            {lat.toFixed(3)},
-            {lng.toFixed(3)}
-          </p>
-          <Weather lat={lat} lng={lng} />
-        </div>
-      );
+      alert('EI TOIMI!')
     }
+  }, [])
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  else {
+    return (
+      <div className="content">
+        <h3>Your position</h3>
+        <p>
+          Position:&nbsp;
+          {lat.toFixed(3)},
+          {lng.toFixed(3)}
+        </p>
+        <Weather lat={lat} lng={lng} />
+      </div>
+    );
+  }
 }
 
 export default App;
